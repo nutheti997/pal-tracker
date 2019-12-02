1af6c4acb23506218a217576a6a4ff9678d7c4e0
pal_user <someuser@someemail.com>
2019-12-02 11:30:59 -0700

    pivotal code commit

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+}
